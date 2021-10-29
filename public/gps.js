70dd77da02d92ad22a9e993e4901edfdaf34b994
Wessel van den Ijssel <wessel069@gmail.com>
2021-10-29 13:15:15 +0200

    camera

--- public/gps.js
+++ public/gps.js
@@ -16,3 +16,29 @@ function getLocation() {
   }
   //request for location
   getLocation();
+  function locationError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            return "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            return "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            return "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            return "An unknown error occurred."
+            break;
+    }
+}
+navigator.geolocation.getCurrentPosition((position) => {
+  console.log("latitude: " + position.coords.latitude)
+  console.log("longitude: " + position.coords.longitude)
+  console.log("speed: " + position.coords.speed)
+  console.log("accuracy: " + position.coords.accuracy)
+  console.log("heading: " + position.coords.heading)
+  document.getElementById("location").innerHTML = "latitude: " + position.coords.latitude + "<br>"
+  + "longitude: " + position.coords.longitude + "<br>" + "speed: " + position.coords.speed + "<br>" 
+  + "accuracy: " + position.coords.accuracy + "<br>" + "heading: " + position.coords.heading;
+})
