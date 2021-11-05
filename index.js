5697a2f43ef4be8bd1489ebe7092bb2c3a9475e2
Wessel van den Ijssel <wessel069@gmail.com>
2021-11-05 13:31:29 +0100

    gps

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 var app = require('express')()
 const PORT = process.env.PORT || 3001;
-var io  = require('socket.io').listen(server);
  
 app.use(express.static('public'));
  
 app.get('/', (req, res) => {
     res.send('Hello World!');
-});
-io.sockets.on('connection', function(socket){
-    socket.on('orientationEvent', function(data) {
-        //console.log(data)
-        socket.broadcast.emit('update_orientationEvent', data);
-    })
 });
 
  
